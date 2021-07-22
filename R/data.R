fec60b7738e8c686c3ad1dae82b1f741f38868bb
Benjamin Butler <benjamin.butler@hutton.ac.uk>
2021-07-22 16:51:08 +0100

    Documenting data

--- R/data.R
+++ R/data.R
@@ -17,3 +17,24 @@
 #'
 "scotland_xrpd"
 
+#' Martian XRPD data from NASA's Mars Science Laboratory
+#'
+#' A multiXY object of 31 XRPD patterns recorded on the Mars Science Laboratory
+#' aboard NASA's Curiosity Rover. Data were obtained from the Geosciences Data Volume
+#' Online (https://pds-geosciences.wustl.edu/msl/msl-m-chemin-4-rdr-v1/) on 14.07.21.
+#'
+#' @format A multiXY object
+#'
+"mars_xrpd"
+
+#' Product IDs and site names of Mars Science Laboratory XRPD data
+#'
+#' Product IDs and site names of 31 XRPD patterns recorded on the Mars Science Laboratory
+#' aboard NASA's Curiosity Rover. Data were obtained from the Geosciences Data Volume
+#' Online (https://pds-geosciences.wustl.edu/msl/msl-m-chemin-4-rdr-v1/) on 14.07.21.
+#'
+#' @format A data frame
+#'
+"mars_id"
+
+
