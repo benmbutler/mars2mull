2d7c9ad1346fad271d3c5df52cb64fe95b35bd58
Benjamin Butler <benjamin.butler@hutton.ac.uk>
2021-07-22 15:20:00 +0100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Profile locations of the Scottish soil samples
+#'
+#' Sample IDs, profile IDs, profile longitude and profile latitude of the
+#' Scottish soil samples.
+#'
+#' @format A data frame
+#'
+"scotland_locations"
+
+#' Scottish XRPD data
+#'
+#' A data frame of 781 columns containing XRPD data. First column is the
+#' 2theta axis, followed by 781 columns of count intensities, with sample IDs
+#' as column names.
+#'
+#' @format A data frame
+#'
+"scotland_xrpd"
+
